2c3abb0e6f1c1095120caf4c3b8adebf41917e19
Linson Butts <linson.butts@students.cau.edu>
2019-09-14 19:31:53 -0400

    project2 first commit

---
+++ apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Appointment.java
@@ -0,0 +1,20 @@
+package edu.cau.cps.cis301.linsonbutts;
+
+import edu.pdx.cs410J.AbstractAppointment;
+
+public class Appointment extends AbstractAppointment {
+  @Override
+  public String getBeginTimeString() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public String getEndTimeString() {
+    throw new UnsupportedOperationException("This method is not implemented yet");
+  }
+
+  @Override
+  public String getDescription() {
+    return "This method is not implemented yet";
+  }
+}
