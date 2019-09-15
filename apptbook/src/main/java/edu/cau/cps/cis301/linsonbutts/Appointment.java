09e4ebd6c112a4d1eda7df9af2097fbbffd02fea
Linson Butts <linson.butts@students.cau.edu>
2019-09-14 20:06:00 -0400

    first commit

--- apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Appointment.java
+++ apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Appointment.java
@@ -1,8 +1,8 @@
 package edu.cau.cps.cis301.linsonbutts;
 
 import edu.pdx.cs410J.AbstractAppointment;
-
 public class Appointment extends AbstractAppointment {
+
   @Override
   public String getBeginTimeString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
--- apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Project2.java
+++ apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Project2.java
@@ -3,7 +3,7 @@ package edu.cau.cps.cis301.linsonbutts;
 /**
  * The main class for the CS410J appointment book Project
  */
-public class Project1 {
+public class Project2 {
 
   public static void main(String[] args) {
     Appointment appointment = new Appointment();  // Refer to one of Dave's classes so that we can be sure it is on the classpath
