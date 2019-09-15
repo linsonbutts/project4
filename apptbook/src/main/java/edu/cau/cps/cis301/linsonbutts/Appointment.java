dad6df2ca9de7438e3c151a4de45b3c53f4f581e
Linson Butts <linson.butts@students.cau.edu>
2019-09-14 20:19:40 -0400

    second commit added accessors and mutator methods

--- apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Appointment.java
+++ apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/Appointment.java
@@ -2,7 +2,13 @@ package edu.cau.cps.cis301.linsonbutts;
 
 import edu.pdx.cs410J.AbstractAppointment;
 public class Appointment extends AbstractAppointment {
-
+  private String descript;
+  public Appointment(String descript){
+    this.descript = descript;
+  }
+    public void setDescript(String descript){
+    this.descript = descript;
+    }
   @Override
   public String getBeginTimeString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
@@ -15,6 +21,6 @@ public class Appointment extends AbstractAppointment {
 
   @Override
   public String getDescription() {
-    return "This method is not implemented yet";
+    return descript;
   }
 }
--- apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/AppointmentBook.java
+++ apptbook/src/main/java/edu/cau/cps/cis301/linsonbutts/AppointmentBook.java
@@ -3,21 +3,34 @@ package edu.cau.cps.cis301.linsonbutts;
 import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class AppointmentBook extends AbstractAppointmentBook {
+    private String owner;
+    private ArrayList<String> appointments;
+
+    public AppointmentBook(String owner, ArrayList<String>appointments){
+        this.owner = owner;
+        this.appointments = appointments;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
     @Override
     public String getOwnerName() {
-        return null;
+        return owner;
     }
 
     @Override
     public Collection getAppointments() {
-        return null;
+        return appointments;
     }
 
     @Override
     public void addAppointment(AbstractAppointment abstractAppointment) {
-
+        this.appointments.add(appt);
     }
 }
